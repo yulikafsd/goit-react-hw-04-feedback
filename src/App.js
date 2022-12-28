b8ee00e37aff85b6b0395c76528d0b385b7e755a
Yulika Zagorovsky <ju.zagorovsky@gmail.com>
2022-12-28 18:08:08 +0100

    refactor to hooks (2 options)

--- src/App.js
+++ src/App.js
@@ -1,66 +1,88 @@
-import { Component } from 'react';
+import { useReducer } from 'react';
 import { Box } from 'styles';
 import { Section, FeedbackOptions, Statistics, Notification } from 'components';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const initialValue = { good: 0, bad: 0, neutral: 0 };
+
+function countReducer(state, action) {
+  return { ...state, [action]: state[action] + 1 };
+}
+
+export function App() {
+  const [state, dispatch] = useReducer(countReducer, initialValue);
+  const { good, bad, neutral } = state;
+
+  const countTotalFeedback = () => {
+    return Object.values(state).reduce((acc, value) => acc + value, 0);
   };
 
-  onLeaveFeedback = name => {
-    this.setState(prevState => {
-      return { [name]: prevState[name] + 1 };
-    });
+  const countPositiveFeedbackPercentage = () => {
+    return totalFeedback ? Math.round((good * 100) / totalFeedback) : 0;
   };
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
+  // Другий варіант - без редьюса
+  // розкоментувати внизу, закоментувати зверху до імпортів + імпорт редьюса
 
-  countPositiveFeedbackPercentage = () => {
-    const total = this.countTotalFeedback();
-    return total
-      ? Math.round((this.state.good * 100) / total)
-      : this.initialValue;
-  };
+  // import { useState } from 'react';
+  // export function App() {
+  //   const initialValue = 0;
 
-  render() {
-    const {
-      state,
-      onLeaveFeedback,
-      countTotalFeedback,
-      countPositiveFeedbackPercentage,
-    } = this;
-    const { good, neutral, bad } = this.state;
+  //   const [good, setGood] = useState(() => initialValue);
+  //   const [bad, setBad] = useState(() => initialValue);
+  //   const [neutral, setNeutral] = useState(() => initialValue);
 
-    const totalFeedback = countTotalFeedback();
-    const positivePercentage = countPositiveFeedbackPercentage();
+  //   const state = { good, bad, neutral };
 
-    return (
-      <Box width="500px" color="text" fontSize={4} as="header" p="0px 30px">
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(state)}
-            onLeaveFeedback={onLeaveFeedback}
+  //   const dispatch = option => {
+  //     switch (option) {
+  //       case 'good':
+  //         setGood(good => good + 1);
+  //         break;
+  //       case 'bad':
+  //         setBad(bad => bad + 1);
+  //         break;
+  //       case 'neutral':
+  //         setNeutral(neutral => neutral + 1);
+  //         break;
+  //       default:
+  //         throw new Error(`Sorry, an error occured`);
+  //     }
+  //   };
+
+  //   const countTotalFeedback = () => {
+  //     return good + neutral + bad;
+  //   };
+
+  //   const countPositiveFeedbackPercentage = () => {
+  //     return totalFeedback
+  //       ? Math.round((good * 100) / totalFeedback)
+  //       : initialValue;
+  //   };
+
+  const totalFeedback = countTotalFeedback();
+  const positivePercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <Box width="500px" color="text" fontSize={4} as="header" p="0px 30px">
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys(state)}
+          onLeaveFeedback={dispatch}
+        />
+      </Section>
+      <Section title="Statistics">
+        {!totalFeedback ? (
+          <Notification message="There is no feedback"></Notification>
+        ) : (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positivePercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {!totalFeedback ? (
-            <Notification message="There is no feedback"></Notification>
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback}
-              positivePercentage={positivePercentage}
-            />
-          )}
-        </Section>
-      </Box>
-    );
-  }
+        )}
+      </Section>
+    </Box>
+  );
 }
